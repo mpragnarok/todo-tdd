3867062d8340b75093c27129f5a867988d35ea2f
mina.h <mina.h@amaryllo.us>
2020-08-13 14:40:44 +0800

    <Unit-test><init project and Create first unit test>

---
+++ controllers/todo.controller.js
@@ -0,0 +1,6 @@
+const TodoModel = require("../model/todo.model");
+
+exports.createTodo = (req, res, next) => {
+    const createModel = TodoModel.create(req.body);
+    res.status(201).json(createModel);
+};
