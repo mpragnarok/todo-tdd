6b389b0fd13f14fd1af927f4a637f4f857dd3371
mina.h <mina.h@amaryllo.us>
2020-08-13 17:06:39 +0800

    <test><createTodo><Add integration test and UT>
    - Handle error message in Integration Testing and Unit testing
    - Seperate server from app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(express.json());
 
 app.use("/todos", todoRoutes);
 
+app.use((error, req, res, next) => {
+    res.status(500).json({ message: error.message });
+});
+
 app.get("/", (req, res) => {
     res.json("Hello world!");
 });
-
-// app.listen(3000, () => {
-//     console.log("Server is now running!");
-// });
 
 module.exports = app;
